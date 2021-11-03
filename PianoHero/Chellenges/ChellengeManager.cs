55ab3e40290b304d4e3e08de7cabab88e5c00b68
ShaKann <pawel.zawadzki@britishcouncil.org>
2021-11-03 22:16:04 +0100

    Initial commit

---
+++ PianoHero/Chellenges/ChellengeManager.cs
@@ -0,0 +1,36 @@
+﻿using Melanchall.DryWetMidi.MusicTheory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PianoHero.Chellenges
+{
+    public class ChellengeManager
+    {
+        private Queue<ChellengeTask> tasks;
+
+        public ChellengeManager(ChellengeDefinition definition)
+        {
+            Definition = definition;
+            this.tasks = new Queue<ChellengeTask>(this.Definition.GetTasks());
+        }
+
+        public ChellengeDefinition Definition { get; }
+ 
+    }
+
+    public class ChellengeTask
+    {
+        public Note[] ExpectedNotes { get; set; }
+    }
+
+    public abstract class ChellengeDefinition
+    {
+        internal ChellengeTask[] GetTasks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PianoHero/NoteManager.cs
@@ -0,0 +1,25 @@
+﻿using Melanchall.DryWetMidi.MusicTheory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PianoHero
+{
+    public class NoteManager
+    {
+        private Dictionary<string, int>  intervals = new Dictionary<string, int> {
+            { "Oct", 12},
+            { "P5", 7},
+            { "M3", 4},
+            { "m3", 3}
+        };
+
+        private List<NoteName> possibleNotes = new List<NoteName> { 
+            NoteName.C,
+            NoteName.D,
+            NoteName.E
+        };
+    }
+}
