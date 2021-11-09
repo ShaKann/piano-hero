24d2310f05b8fb7769a96fa52efd480863fc3b75
ShaKann <pawel.zawadzki@britishcouncil.org>
2021-11-09 10:18:03 +0100

    Current

---
+++ PianoHero/Infrastructure/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using Melanchall.DryWetMidi.MusicTheory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PianoHero.Infrastructure
+{
+    public interface IInputManager
+    {
+        void Start(Action<Note[]> callback);
+    }
+
+    public class InputManager : IInputManager
+    {
+        public void Start(Action<Note[]> callback)
+        {
+            
+        }
+    }
+}
---
+++ PianoHero/Infrastructure/OutputManager.cs
@@ -0,0 +1,30 @@
+﻿using Melanchall.DryWetMidi.MusicTheory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PianoHero.Infrastructure
+{
+    public interface IOutputManager
+    {
+        void SendNote(params Note[] notes);
+    }
+
+    public class StringOutputManager : IOutputManager
+    {
+        private readonly Action<string> printAction;
+
+        public StringOutputManager(Action<string> printAction)
+        {
+            this.printAction = printAction;
+        }
+
+        public void SendNote(params Note[] notes)
+        {
+            var noteString = $"{string.Join(" ", notes.Select(x => x.ToString()))}";
+            this.printAction(noteString);
+        }
+    }
+}
